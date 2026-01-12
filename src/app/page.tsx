2d0414a8347cd9d7e65f50e20544a7a58b65befd
Anita Baron <anita.baron@i4b.pl>
2026-01-12 12:39:34 +0100

    feature: supabase initialized, migrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <DbTest />
+        {/* <DbTest /> */}
       </header>
 
       <main className="mx-auto w-full max-w-5xl px-6 py-10 sm:px-10">
