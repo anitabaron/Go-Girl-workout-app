b5c5f6a8f38148dd2ae22f8a8cb0c0ffabb69d8d
Anita Baron <anita.baron@i4b.pl>
2026-01-30 15:13:29 +0100

    chore: update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -69,13 +69,7 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    String.raw`/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)`,
+    // Match all request paths except: _next/static, _next/image, favicon.ico, image files
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
