8a84747198b0ec3ed57d305045340a8a453922e9
Anita Baron <anita.baron@i4b.pl>
2026-02-11 16:04:50 +0100

    remove legacy app routes

--- src/app/(app)/(legacy)/legacy/workout-plan/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import { requireAuth } from "@/lib/auth";
-
-export default async function TestPage() {
-  // Weryfikacja autoryzacji - automatyczne przekierowanie niezalogowanych użytkowników
-  await requireAuth();
-
-  return <div>Test Page</div>;
-}
